d4ed31b977d6289bc5da97fb5664b65fa339af16
Ydalirsson <pantherkoenig@gmail.com>
2021-05-05 01:07:37 +0200

    first version

---
+++ vue.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  pluginOptions: {
+    electronBuilder: {
+      nodeIntegration: true
+    },
+    builderOptions: {
+      win: {
+        icon: 'src/assets/icon.png'
+      }
+    }
+  }
+}
