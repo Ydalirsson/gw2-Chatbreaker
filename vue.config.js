21ff5224c1d9c01129e479dbcaf5a4d3abcc1abd
Ydalirsson <pantherkoenig@gmail.com>
2024-04-03 23:58:52 +0200

    Update vue.config.js

--- vue.config.js
+++ vue.config.js
@@ -2,6 +2,6 @@ const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
   publicPath: process.env.NODE_ENV === 'production'
-  ? '/gw2-Chatbreaker /'
+  ? '/gw2-Chatbreaker/'
   : '/'
 })
