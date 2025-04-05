d9c0dc34feb767aec3c821abedcf7c0fb2ba0442
Ydalirsson <24847261+Ydalirsson@users.noreply.github.com>
2025-04-05 17:44:39 +0200

    refactored TableResults

--- src/components/Main.vue
+++ src/components/Main.vue
@@ -7,7 +7,7 @@
     <SearchBar :insertTextAtCursor="insertTextAtCursor" />
     <p></p>
     <br />
-    <!-- <ResultsTable :messages="singleMessage" /> -->
+    <ResultsTable :singleMessages="singleMessages" />
   </div>
 </template>
 
@@ -18,9 +18,8 @@ import TextEditor from "./TextEditor.vue";
 import Toolbar from "./Toolbar.vue";
 import EmoteControls from "./EmoteControls.vue";
 import SearchBar from "./SearchBar.vue"
-/*
 import ResultsTable from "./ResultsTable.vue";
-*/
+
 export default defineComponent({
   name: 'Main',
   components: {
@@ -29,12 +28,12 @@ export default defineComponent({
     Toolbar,
     EmoteControls,
     SearchBar,
-   // ResultsTable
+    ResultsTable
   },
   data() {
     return {
       chatContent: "" as string,
-      singleMessage: [] as Array<{ text: string; copied: false }>,
+      singleMessages: [] as Array<{ text: string; copied: false }>,
       contentWordArray: [""] as Array<string>,
       errorMessage: "" as string,
       selected: 1 as number,
@@ -56,7 +55,7 @@ export default defineComponent({
       let i = 0; // place of message
       let j = 0; // position of word
       // reset
-      this.singleMessage = [{ text: "", copied: false }];
+      this.singleMessages = [{ text: "", copied: false }];
       this.contentWordArray = [""];
       this.errorMessage = "";
 
@@ -85,60 +84,60 @@ export default defineComponent({
           // check if word is an emote
           if (this.contentWordArray[j] === "/emote" || this.contentWordArray[j] === "/e" || this.contentWordArray[j] === "/em" || this.contentWordArray[j] === "/me") {
             // If it's an emote, start a new message and append "/e" to it
-            if (this.singleMessage[i].text.length > 0) {
-              this.singleMessage[i].text += separatorChar; // finish current message
-              this.singleMessage.push({ text: "", copied: false }); // start a new message
+            if (this.singleMessages[i].text.length > 0) {
+              this.singleMessages[i].text += separatorChar; // finish current message
+              this.singleMessages.push({ text: "", copied: false }); // start a new message
               i++;
             }
-            this.singleMessage[i].text += "/e ";
+            this.singleMessages[i].text += "/e ";
             j++;
 
             // Process subsequent words until "##" is found
             while (j < this.contentWordArray.length && this.contentWordArray[j] !== "##") {
               // Check if adding the word exceeds the character limit
-              if (this.singleMessage[i].text.length + this.contentWordArray[j].length <= MSG_CHAR_LIMIT) {
-                this.singleMessage[i].text += this.contentWordArray[j].toString() + " ";
+              if (this.singleMessages[i].text.length + this.contentWordArray[j].length <= MSG_CHAR_LIMIT) {
+                this.singleMessages[i].text += this.contentWordArray[j].toString() + " ";
               } else {
                 // If exceeds the limit, start a new message
-                this.singleMessage[i].text += separatorChar; // finish Message
-                this.singleMessage.push({ text: "", copied: false }); // init next message
+                this.singleMessages[i].text += separatorChar; // finish Message
+                this.singleMessages.push({ text: "", copied: false }); // init next message
                 i++;
                 j--;
-                this.singleMessage[i].text += "/e "; // Start a new message with "/e"
+                this.singleMessages[i].text += "/e "; // Start a new message with "/e"
               }
               j++;
             }
 
             if (this.contentWordArray[j] === "##") {
               // If it's the stop signal, start a new message
-              this.singleMessage[i].text += separatorChar; // finish current message
-              this.singleMessage.push({ text: "", copied: false }); // start a new message
+              this.singleMessages[i].text += separatorChar; // finish current message
+              this.singleMessages.push({ text: "", copied: false }); // start a new message
               i++;
             }
 
           }
           else      // check if word is an emote
             if (this.contentWordArray[j].startsWith("/")) {
-              if (this.singleMessage[i].text.length > 0) {
-                this.singleMessage[i].text += separatorChar; // finish current message
-                this.singleMessage.push({ text: "", copied: false }); // start a new message
+              if (this.singleMessages[i].text.length > 0) {
+                this.singleMessages[i].text += separatorChar; // finish current message
+                this.singleMessages.push({ text: "", copied: false }); // start a new message
                 i++;
               }
-              this.singleMessage[i].text += this.contentWordArray[j] + " "; // add slash command to current message
-              this.singleMessage.push({ text: "", copied: false }); // start a new message
+              this.singleMessages[i].text += this.contentWordArray[j] + " "; // add slash command to current message
+              this.singleMessages.push({ text: "", copied: false }); // start a new message
               i++;
             }
             else if (
-              this.singleMessage[i].text.length +
+              this.singleMessages[i].text.length +
               this.contentWordArray[j].length <=
               MSG_CHAR_LIMIT
             ) {
-              this.singleMessage[i].text +=
+              this.singleMessages[i].text +=
                 this.contentWordArray[j].toString() + " ";
               //console.log('Msg: ' + this.singleMessage[0])
             } else {
-              this.singleMessage[i].text += separatorChar; // finish Message
-              this.singleMessage.push({ text: "", copied: false }); // init next message
+              this.singleMessages[i].text += separatorChar; // finish Message
+              this.singleMessages.push({ text: "", copied: false }); // init next message
               i++;
               j--; // hold current word to set in next message
             }
---
+++ src/components/ResultsTable.vue
@@ -0,0 +1,61 @@
+<template>
+    <div class="table-responsive">
+        <table class="table table-dark table-striped">
+            <tbody>
+                <tr v-for="msg in singleMessages" v-bind:key="msg.text">
+                    <td>{{ msg.text }}</td>
+                    <td>
+                        <button type="button" @click="onCopy(msg)" v-if="msg.text" class="btn btn-outline-info">
+                            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor"
+                                class="bi bi-clipboard" viewBox="0 0 16 16">
+                                <path
+                                    d="M4 1.5H3a2 2 0 0 0-2 2V14a2 2 0 0 0 2 2h10a2 2 0 0 0 2-2V3.5a2 2 0 0 0-2-2h-1v1h1a1 1 0 0 1 1 1V14a1 1 0 0 1-1 1H3a1 1 0 0 1-1-1V3.5a1 1 0 0 1 1-1h1v-1z">
+                                </path>
+                                <path
+                                    d="M9.5 1a.5.5 0 0 1 .5.5v1a.5.5 0 0 1-.5.5h-3a.5.5 0 0 1-.5-.5v-1a.5.5 0 0 1 .5-.5h3zm-3-1A1.5 1.5 0 0 0 5 1.5v1A1.5 1.5 0 0 0 6.5 4h3A1.5 1.5 0 0 0 11 2.5v-1A1.5 1.5 0 0 0 9.5 0h-3z">
+                                </path>
+                            </svg>
+                            Copy
+                        </button>
+                    </td>
+                    <td v-if="msg.copied">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" fill="green" class="bi bi-check"
+                            viewBox="0 0 16 16">
+                            <path
+                                d="M10.97 4.97a.75.75 0 0 1 1.07 1.05l-3.99 4.99a.75.75 0 0 1-1.08.02L4.324 8.384a.75.75 0 1 1 1.06-1.06l2.094 2.093 3.473-4.425a.267.267 0 0 1 .02-.022z" />
+                        </svg>
+                    </td>
+                </tr>
+            </tbody>
+        </table>
+    </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+
+export default defineComponent({
+    name: "ResultsTable",
+    props: {
+        singleMessages: {
+            type: Array as () => Array<{ text: string; copied: boolean }>,
+            required: true,
+        }
+    },
+    data() {
+        return {
+        }
+    },
+    methods: {
+        onCopy(msg: { text: string, copied: boolean }): void {
+            //console.log("CopyText" + msg);
+            navigator.clipboard.writeText(msg.text);
+            msg.copied = true;
+        }
+    }
+
+});
+
+</script>
+
+<style lang="css"></style>
